bb6e94dc0cf68c60eb5f7088ac5737b5f6f4daa5
Ryoki Tanaka <ryoki0525tnk@gmail.com>
2023-09-28 03:44:28 +0900

    feat: dockerコンテナ内でtelnetを実行する
    ホストからコンテナにtelnetで接続すると接続完了してすぐに切断されてしまうので、ワークアラウンドとして。

--- files/scripts/expect/list_players.sh
+++ files/scripts/expect/list_players.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/expect
 
 set timeout 5
-spawn telnet localhost 8081
+spawn docker exec -it 7dtdserver telnet localhost 8081
 send "listplayers\r"
 expect -re "in the game"
 send "exit\r"
